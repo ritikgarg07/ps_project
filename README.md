8e953f4f98943b1c1472379bd82ca07df154894c
ritikgarg07 <ritikgarg07@gmail.com>
2020-06-08 13:29:36 +0000

    added unet model layout

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 ## ps_project
 # PS Project for summer 2020 at IIRS
 
-### Setup instructions (after setting up environment)
+### Setup instructions (after setting up environment)  
 
 `git clone https://github.com/ritikgarg07/ps_project.git`  
-`cd ps_project`    
+`cd ps_project`  
+`sh scripts/setup.sh` (This will create a docker image)      
 `sh scripts/download.sh -d -u`  
+  
 NOTE: for download.sh 2 flags are available:   
     if -d is provided it will download dataset  
     if -u is provided it will unzip dataset  
--- src/model.py
+++ src/model.py
@@ -1,5 +1,36 @@
-import numpy as np 
-from tensorflow import keras
+# import numpy as np 
+import tensorflow as tf 
+from tensorflow.keras import layers, optimizers
 
-def unet(input_size = (32, 32, 3)):
-    pass
+batch_size = 10
+patch_size = 32
+
+def unet(input_size = (patch_size, patch_size, 3)):
+
+    rgb = layers.Input(input_size)
+   
+    # ENCODER layers 
+    conv1 = layers.Conv2D(filters=32, kernel_size=3, strides=1, activation='relu', padding='valid')(rgb)  
+    conv2 = layers.Conv2D(filters=64, kernel_size=3, strides=1, activation='relu', padding='valid')(conv1)
+    conv3 = layers.Conv2D(filters=128, kernel_size=3, strides=1, activation='relu', padding='valid')(conv2)
+    conv4 = layers.Conv2D(filters=128, kernel_size=3, strides=1, activation='relu', padding='valid')(conv3)
+    
+    # Decoder layers
+    upconv1 = layers.Conv2DTranspose(filters=128, kernel_size=3, strides=1, activation='relu', padding='valid')(conv4)
+    merge1 = layers.concatenate([upconv1, conv3], axis = 3)
+    
+    upconv2 = layers.Conv2DTranspose(filters=64, kernel_size=3, strides=1, activation='relu', padding='valid')(merge1)
+    merge2 = layers.concatenate((upconv2, conv2), axis=3)
+    
+    upconv3 = layers.Conv2DTranspose(filters=32, kernel_size=3, strides=1, activation='relu', padding='valid')(merge2)
+    merge3 = layers.concatenate((upconv3, conv1), axis=3)
+    
+    # output layer, filters = number of bands
+    hyper = layers.Conv2DTranspose(filters=31, kernel_size=3, strides=1, activation='relu', padding='valid')(merge3)
+    
+
+    model = tf.keras.Model(inputs = rgb, outputs = hyper)
+    model.compile(optimizer = optimizers.Adam(learning_rate = 0.0001), loss = 'mse', metrics = 'mse')
+
+    # print(model.summary())
+    return model
