49c038ac62540447925207a44a425c2687e6cec8
ritikgarg07 <ritikgarg07@gmail.com>
2020-06-08 11:19:23 +0000

    updated download.sh for appropriate renaming of dataset

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,10 +1,46 @@
 #!bin/bash
 
-wget -O ./data/data.zip https://www.cs.columbia.edu/CAVE/databases/multispectral/zip/complete_ms_data.zip
+while getopts ":du" opt; do
+    case $opt in
+    d)
+        echo "-d was triggered! Downloading dataset"
+        wget -O ./data/data.zip https://www.cs.columbia.edu/CAVE/databases/multispectral/zip/complete_ms_data.zip      
+        ;;
+    u) 
+        echo "-u was triggered! Unzipping dataset"
+        unzip data.zip -d ./data/
+        ;;
+    \?)
+        echo "Invalid option! No flags required if data already placed correctly"
+        ;;
+    esac
+done
 
-unzip data.zip
 
+cd ./data
 
-
+i=0
+for f in */; do
+    cd $f
+    mv -v ./$f/* .
+    rm -rf $f
+    for f2 in *; do
+        ext="${f2##*.}"
+        if [ "$ext" = "db" ]; then
+            mv $f2 $i.$ext
+        elif [ "$ext" = "bmp" ]; then
+            mv $f2 $i.$ext
+        else
+            name=`expr "/$f2" : '.*\(.\{2\}\)\.'`
+            mv $f2 "${i}_${name}.${ext}"
+        fi
+    done
+    cd ..
+    mv $f $i
+    echo $i
+    i=$((i+1))
+done
+touch .gitkeep
 # uncomment if you want to delete zip after extraction for space purposes
-# rm data.zip
+cd .. 
+rm data.zip
---
+++ src/model.py
@@ -0,0 +1,5 @@
+import numpy as np 
+from tensorflow import keras
+
+def unet(input_size = (32, 32, 3)):
+    pass
