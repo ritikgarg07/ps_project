a9a6723997133a7ceec539a8be1ce11ac6d05cc2
ritikgarg07 <ritikgarg07@gmail.com>
2020-06-10 16:41:07 +0000

    rewrote dataset.py, performance benchmarking left

--- src/DataSet.py
+++ src/DataSet.py
@@ -1,97 +1,96 @@
 import numpy as np 
 import tensorflow as tf
+import tensorboard
 import os
+
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
+# Class for dataset
 class DataSet(object):
     def __init__(self, batch_size, path = '/workspaces/ps_project/data/'):
         self.batch_size = batch_size
-        # self.dataset = dataset
-        # self.train = train
-        # self.validation = validation
-        # self.test = test
         self.path = path
     
     # Takes the file_path to a bmp(RGB) input and outputs the image name
     def __get_id(self, file_path):
-        parts = tf.strings.split(file_path, os.path.sep)
+        parts = file_path.split('/')
         id = parts[-1]
-        id = tf.strings.split(id, '.')
+        id = id.split('.')
         id = id[0]
-        return id
+        return id    
     
-    # Takes the file_path to a bmp(RGB) input and outputs the image as a tensor of shape (im_size, im_size, 3)
-    # ! Normalises uses implicit normalisation from uint8 to float 32 ie- [0,255] to [0,1]
+    # Takes the file_path to a bmp(RGB) input and returns the image as a tensor of shape (32, 32, 3)
+    # ! Normalises using implicit normalisation from uint8 to float32 (to [0,1])
     def __decode_input(self, file_path):
         img = tf.io.read_file(file_path)
         img = tf.image.decode_image(img, channels=3)
-        img = tf.image.convert_image_dtype(img, tf.float32)
+        img = tf.image.convert_image_dtype(img, dtype = tf.dtypes.float32)
+        img = tf.reshape(img, (32, 32, 3))
         return img
 
-    # Takes the file_path to a png(Hyperpectral) output image and outputs the image as a tensor of shape (im_size, im_size, 1)
-    # ! No normalisation done for now, returns tensor of dtype uint8
-    # TODO: Implement normalisation
-    # TODO: Lookup channels for png image
+    # Takes file_path to a png(Hyper) output image and returns the image as a tensor of shape (32, 32, 1)
+    # ! Normalises using implicit normalisation from uint16 to float32 (to [0,1])
     def __decode_output(self, file_path):
         img = tf.io.read_file(file_path)
-        img = tf.image.decode_image(img)
-        img = tf.image.convert_image_dtype(img, dtype=tf.uint8)
+        img = tf.image.decode_png(img, channels=1, dtype = tf.dtypes.uint16)
+        img = tf.image.convert_image_dtype(img, dtype = tf.dtypes.float32)
         return img 
 
-    # Wrapper function
-    # Takes file_path to a bmp(RGB) input image and returns a tuple of input image, output image (stacked)
-    # (tensor of shape (32, 32, 3), tensor of shape (32, 32, 31))
-    def __process_ds(self, file_path):
+    # Takes the list of file_paths corr. to a input image and outputs the corr. output stacked image as a tensor of shape (32, 32, 31)
+    def __parse_output(self, path_list):
+        tensor_list = [self.__decode_output(file_path) for file_path in path_list]
+        op = tf.stack(tensor_list, axis = 2)
+        op = tf.reshape(op, (32, 32, 31))
+        return op
         
-        ip = self.__decode_input(file_path)    # input image tensor
-
-        # Get input image name
-        id_image = self.__get_id(file_path)
-        id_string = str(id_image)
-        id_string = id_string[id_string.find("'")+1:]
-        id_string = id_string[: id_string.find("'")]
-        id_string += '_'
-        
-        # List of output image file_paths for the input image
-        list_op = tf.io.gfile.glob(self.path + id_string + '*.png')
-        
-        # A list of output image tensors for input images
-        # ! uint8 should match with type in decode_output
-        images_op = [tf.zeros((32,32,1), dtype = tf.uint8) for i in range(31)]
-        for index, file_path in enumerate(list_op):
-            images_op[index] = self.__decode_output(file_path)
-        
-        # Generate single tensor of shape (32, 32, 31) from 31 tensors of shape (32, 32)
-        op = tf.stack(images_op, axis = 2)
-        op = tf.reshape(op, (32,32,31))
-
-        return (ip, op)
-
+    # Main function
     def load_process(self, shuffle_buffer_size = 1000, train = False, test = False, validation = False):
-        # TODO: Implement test and validation 
-
+        
         if (not train) & (not test) & (not validation):
             print('Error! Please select one')
+            return -1
         if train:
-            path = self.path + 'train/'
-        
+            folder = 'train/'
         if validation:
-            path = self.path + 'validation/'
+            folder = 'validation/'
+        if test:
+            folder = 'test/'
 
-        # List of input images in given folder defined by path variable
-        self.list_ds = tf.data.Dataset.list_files(str(path + '*.bmp'))
+        # List of paths to input images in given folder defined by path variable
+        self.list_ip = tf.io.gfile.glob(str(self.path + folder + '*.bmp'))
 
-        # List of tuples (input, output) in given folder defined by path variable
-        self.labelled_ds = self.list_ds.map(self.__process_ds, num_parallel_calls=AUTOTUNE)
+        # List of 'ids' of input images ie- 'sampleno_rowno_column_no'
+        self.list_id = list(map(self.__get_id, self.list_ip))
+
+        # List of lists of paths to output images for each input image
+        # Structure: [ [op for ip1], [op for ip2], ...]
+        self.list_op = list(map(lambda f: tf.io.gfile.glob(self.path + folder + f + '*.png'), self.list_id))
+        
+        # List of op stacked images for each ip
+        # Structure: [ tensor(32, 32, 31) for op1, tensor(32, 32, 31 for op2), ...]
+        # Decodes op images from paths to tensors
+        self.tensor_list = [self.__parse_output(f) for f in self.list_op[:2]]
+    
+        # Temp dataset of only ip paths
+        self.ds1 = tf.data.Dataset.from_tensor_slices(self.list_ip)
+        self.ds1 = self.ds1.cache()
+        # Decoding ip images from paths to tensors
+        self.ds1 = self.ds1.map(self.__decode_input)
+
+        # Temp dataset of only op tensors (has been parsed from images to tensors at tensor_list)
+        self.ds2 = tf.data.Dataset.from_tensor_slices(self.tensor_list)
+        
+        # Final dataset: tuple of ip_tensor, op_tensor
+        # ie- (tensor of shape (32, 32, 3), tensor of shape(32, 32, 31))
+        self.ds = tf.data.Dataset.zip((self.ds1, self.ds2))
+
         # labelled_ds = self.labelled_ds.cache()
-
         # self.labelled_ds = self.labelled_ds.shuffle(buffer_size = shuffle_buffer_size)
-        # self.labelled_ds = self.labelled_ds.repeat()
-        self.labelled_ds = self.labelled_ds.batch(self.batch_size)
+        self.ds = self.ds.batch(self.batch_size)
+        self.ds = self.ds.prefetch(buffer_size=AUTOTUNE)
 
-        self.labelled_ds = self.labelled_ds.prefetch(buffer_size=AUTOTUNE)
-        return self.labelled_ds
-
+        return self.ds
+    
     def get_batch(self):
-        return next(iter(self.labelled_ds))
+        return next(iter(self.ds))
 
--- src/main.py
+++ src/main.py
@@ -2,15 +2,23 @@ import tensorflow as tf
 import model
 from datetime import datetime
 import DataSet
+import time
 import tensorboard
-
+from benchmarks import timeit
 
 dataset = DataSet.DataSet(batch_size=8)
-train_ds = dataset.load_process(train=True)
-validation_ds = dataset.load_process(validation=True)
+test_ds = dataset.load_process(test=True)
+# train_ds = dataset.load_process(train=True)
+# validation_ds = dataset.load_process(validation=True)
 
 unet = model.unet()
 logdir = '/workspaces/ps_project/logs/fit/' + datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
-history = unet.fit(train_ds, validation_data = validation_ds, batch_size = 8, epochs=2, callbacks=[tensorboard_callback])
+for f in test_ds.take(5):
+    print(f[1])
+
+
+
+
+# history = unet.fit(train_ds, validation_data = validation_ds, batch_size = 8, epochs=2, callbacks=[tensorboard_callback])
