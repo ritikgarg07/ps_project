146075d8e36da4c52e0f25fe8e38b0595ce69ee7
ritikgarg07 <ritikgarg07@gmail.com>
2020-07-01 18:06:37 +0530

    updated run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,3 @@
 IMAGE=ps_image
-CONTAINER=ps_container
-VERSION=0.01
 
-docker run --mount type=bind,source=${PWD}/src,target=/workspaces/ --gpus all -it ${IMAGE}:${VERSION} 
+docker run --mount type=bind,source=${PWD},target=/workspaces/ --gpus all -it ${IMAGE}
